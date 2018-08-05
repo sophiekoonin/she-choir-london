38b7bc38b2d6a93626bc26bca4cd8377ba9bb880
Sophie Koonin <sophie.koonin@gmail.com>
2018-08-05 12:12:22 +0100

    Add copyright notice and make instagram icon super pretty

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import SocialLinks from './navigation/socialLinks'
-
+const currentYear = new Date().getFullYear()
 const Footer = () => (
   <footer className="footer">
     <SocialLinks />
+    <p className="footer__copyright">© 2012-{currentYear} SHE Choir London</p>
   </footer>
 )
 
