7cba9c12617509013cb83c46232c580d346a5a52
Sophie Koonin <sophie.koonin@gmail.com>
2018-08-05 14:01:44 +0100

    Add Jest test and logic for getting calendar events

---
+++ src/utils/calendar.js
@@ -0,0 +1,12 @@
+const { GCAL_API_KEY, GCAL_ID } = process.env
+const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${GCAL_ID}/events?key=${GCAL_API_KEY}`
+
+export const getEvents = async () => {
+  const response = await fetch(calendarUrl)
+  return response.body.items.map(event => ({
+    start: event.start.date || event.start.dateTime,
+    end: event.end.date || event.end.dateTime,
+    title: event.summary,
+    location: event.location
+  }))
+}
---
+++ test/utils/calendar.test.js
@@ -0,0 +1,102 @@
+import { getEvents } from '../../src/utils/calendar'
+
+const response = {
+  body: {
+    kind: 'calendar#events',
+    etag: '"adsfasdf"',
+    summary: 'SHE Choir London Gigs',
+    updated: '2018-07-16T19:02:34.633Z',
+    timeZone: 'Europe/London',
+    accessRole: 'reader',
+    defaultReminders: [],
+    nextSyncToken: 'asdfdasf==',
+    items: [
+      {
+        kind: 'calendar#event',
+        etag: '"asdfadsf"',
+        id: 'mylovelyid',
+        status: 'confirmed',
+        htmlLink: 'https://www.example.com',
+        created: '2016-08-30T15:49:01.000Z',
+        updated: '2016-12-12T16:08:44.359Z',
+        summary: 'Super exciting event name',
+        location: 'West Covina, California',
+        creator: {
+          email: 'someone@example.com',
+          displayName: 'SHE Choir London'
+        },
+        organizer: {
+          email: 'calendar@id',
+          displayName: 'SHE Choir London Gigs',
+          self: true
+        },
+        start: {
+          dateTime: '2016-01-12T18:30:00Z'
+        },
+        end: {
+          dateTime: '2016-01-12T19:30:00Z'
+        }
+      },
+      {
+        kind: 'calendar#event',
+        etag: '"asdfadsf"',
+        id: 'mylovelyid1',
+        status: 'confirmed',
+        htmlLink: 'https://www.example.com',
+        created: '2016-08-30T15:49:01.000Z',
+        updated: '2016-12-12T16:08:44.359Z',
+        summary: 'Another event',
+        location: 'Mordor',
+        creator: {
+          email: 'someone@example.com',
+          displayName: 'SHE Choir London'
+        },
+        organizer: {
+          email: 'calendar@id',
+          displayName: 'SHE Choir London Gigs',
+          self: true
+        },
+        start: {
+          dateTime: '2016-03-13T18:30:00Z'
+        },
+        end: {
+          dateTime: '2016-03-13T19:30:00Z'
+        }
+      },
+      {
+        kind: 'calendar#event',
+        etag: '"asdfadsf"',
+        id: 'mylovelyid2',
+        status: 'confirmed',
+        htmlLink: 'https://www.example.com',
+        created: '2016-08-30T15:49:01.000Z',
+        updated: '2016-12-12T16:08:44.359Z',
+        summary: 'Something else',
+        location: 'Alderaan',
+        creator: {
+          email: 'someone@example.com',
+          displayName: 'SHE Choir London'
+        },
+        organizer: {
+          email: 'calendar@id',
+          displayName: 'SHE Choir London Gigs',
+          self: true
+        },
+        start: {
+          dateTime: '2016-05-14T18:30:00Z'
+        },
+        end: {
+          dateTime: '2016-05-14T19:30:00Z'
+        }
+      }
+    ]
+  }
+}
+
+global.fetch = jest.fn().mockImplementation(() => Promise.resolve(response))
+describe('calendar', () => {
+  it('should map the response items into a slimmed-down array of events', async () => {
+    const events = await getEvents()
+    expect(events).toMatchSnapshot()
+  })
+})
